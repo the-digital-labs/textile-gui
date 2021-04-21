b9f83e53bbe1356a5155cd3110bdc79b778e6283
ChristopherTrimboli <christopher.trimboli@protonmail.com>
2021-04-21 01:35:36 -0700

    instances and collection handlers

---
+++ pages/api/threads/instances/index.ts
@@ -0,0 +1,11 @@
+import { TextileClient, getInstancesByQuery } from "../index";
+import { ThreadID, Query } from "@textile/hub";
+
+export default async function instancesHandler(req, res) {
+    if (req.method === "GET") {
+        const client = await new TextileClient().init();
+        const query = new Query().orderByIDDesc();
+        const instances = await getInstancesByQuery(client, ThreadID.fromString(req.query.ThreadID), req.query.collectionName, query)
+        res.status(200).json(instances);
+    }
+};
