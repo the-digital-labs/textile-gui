2b697f020021342fef9aa1c004df7bb728bfca03
ChristopherTrimboli <christopher.trimboli@protonmail.com>
2021-04-20 19:30:19 -0700

    add Table component starter

---
+++ components/Table.tsx
@@ -0,0 +1,6 @@
+import styles from "../styles/components/Table.module.css";
+import { Table as AntTable } from "antd";
+
+export default function Table({ data = [], columns = [] }) {
+    return <AntTable dataSource={data} columns={columns} />
+};
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Table from "../components/Table";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Head>
         <title>textile-gui</title>
       </Head>
+      <Table />
     </div>
   )
 }
