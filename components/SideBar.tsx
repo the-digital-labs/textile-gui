0123694c1a9691311b029f9ac990196e91fb43f8
ChristopherTrimboli <christopher.trimboli@protonmail.com>
2021-04-22 02:57:58 -0700

    remove collaspe and pagination, style changes

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,27 +1,27 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import styles from "../styles/components/SideBar.module.css";
-import { Collapse, Spin } from 'antd';
+import { Card, Spin } from 'antd';
 import Tree from "./Tree";
 import { AppContext } from "../store/app";
-
-const { Panel } = Collapse;
 
 export default function SideBar({ treeData, buildTree }) {
     const [appCtxState, appCtxActions] = useContext(AppContext);
 
+    useEffect(() => {
+        buildTree()
+    }, [])
+
     return (
-        <Collapse onChange={buildTree}>
-            <Panel header="Tree Explorer" key="1">
-                {
-                    appCtxState.isTreeLoading &&
-                    <div className={styles.spinnerContainer}>
-                        <Spin />
-                    </div>
-                }
-                {
-                    !appCtxState.isTreeLoading && <Tree treeData={treeData} />
-                }
-            </Panel>
-        </Collapse >
+        <Card title="Tree Explorer" className={styles.cardContainer}>
+            {
+                appCtxState.isTreeLoading &&
+                <div className={styles.spinnerContainer}>
+                    <Spin />
+                </div>
+            }
+            {
+                !appCtxState.isTreeLoading && <Tree treeData={treeData} />
+            }
+        </Card>
     );
 };
--- components/Table.tsx
+++ components/Table.tsx
@@ -2,5 +2,5 @@ import styles from "../styles/components/Table.module.css";
 import { Table as AntTable } from "antd";
 
 export default function Table({ data = [], columns = [] }) {
-    return <AntTable dataSource={data} columns={columns} />
+    return <AntTable dataSource={data} columns={columns} pagination={false} />
 };
