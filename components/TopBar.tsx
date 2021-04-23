4741d1efc2b6d95b8de00da3eaaed8fa1fc8e8e7
ChristopherTrimboli <christopher.trimboli@protonmail.com>
2021-04-23 00:29:46 -0700

    top navbar changes, add github link

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,13 +1,16 @@
 import styles from "../styles/components/TopBar.module.css";
 import { Menu } from 'antd';
+import { GithubOutlined } from "@ant-design/icons";
 
 export default function TopBar() {
     return <Menu mode="horizontal">
-        <Menu.Item key="mail">
-            Navigation One
+        <Menu.Item key="title">
+            <h2 style={{ marginBottom: 0 }}>textile-gui</h2>
         </Menu.Item>
-        <Menu.Item key="app">
-            Navigation Two
+        <Menu.Item key="github" style={{ float: "right" }}>
+            <a href="https://github.com/the-digital-labs/textile-gui" target="_blank">
+                <GithubOutlined style={{ fontSize: 20 }} /> Github
+            </a>
         </Menu.Item>
     </Menu>
 };
