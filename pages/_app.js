502ad06f732d555dfbdc3d28f7a58a0914b7700f
ChristopherTrimboli <christopher.trimboli@protonmail.com>
2021-04-22 23:25:53 -0700

    add dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import '../styles/globals.css';
 import 'antd/dist/antd.css';
 import { StoreProvider } from "../store/provider";
+import { DARK_MODE } from "../config.js";
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    if (DARK_MODE) {
+      import("../styles/antd.dark.css");
+    }
+  }, [])
+
   return (
     <StoreProvider>
       <Component {...pageProps} />
