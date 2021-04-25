810de4177bf213f2f0db5be7d6d5b58a014b16c5
ChristopherTrimboli <christopher.trimboli@protonmail.com>
2021-04-24 20:38:58 -0700

    auto expand tree nodes onload

--- components/Tree.tsx
+++ components/Tree.tsx
@@ -11,5 +11,6 @@ export default function Tree({ treeData = [] }) {
         treeData={treeData}
         style={{ height: "100%" }}
         showLine={true}
+        defaultExpandAll={true}
     />
 };
