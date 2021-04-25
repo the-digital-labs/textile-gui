1203e872d11669275c6e5b5142e1fa7a1eba78f7
ChristopherTrimboli <christopher.trimboli@protonmail.com>
2021-04-24 23:09:30 -0700

    add checkbox selection to table

--- components/Table.tsx
+++ components/Table.tsx
@@ -6,12 +6,30 @@ import { AppContext } from "../store/app";
 export default function Table({ data = [], columns = [] }) {
     const [appCtxState, appCtxActions] = useContext(AppContext);
 
+    const rowSelection = {
+        onChange: (selectedRowKeys: React.Key[], selectedRows: []) => {
+            console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+        },
+        getCheckboxProps: (record) => ({
+            disabled: record.name === 'Disabled User', // Column configuration not to be checked
+            name: record.name,
+        }),
+    };
+
     return <>
         {
             appCtxState.isTableLoading && <div style={{ display: "flex", justifyContent: "center", height: "100%" }}><Spin style={{ alignSelf: "center" }} size="large" /></div>
         }
         {
-            !appCtxState.isTableLoading && <AntTable dataSource={data} columns={columns} pagination={false} />
+            !appCtxState.isTableLoading &&
+            <AntTable dataSource={data}
+                columns={columns}
+                pagination={false}
+                rowSelection={{
+                    type: "checkbox",
+                    ...rowSelection,
+                }}
+            />
         }
     </>
 };
