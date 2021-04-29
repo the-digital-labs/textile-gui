e66c1b25ba236fe8827f07eeb36c3f7fdd62699f
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-04-28 21:50:54 -0700

    try - catch instances API

--- pages/api/threads/instances/index.ts
+++ pages/api/threads/instances/index.ts
@@ -1,29 +1,40 @@
-import { TextileClient, getInstancesByQuery, createInstances, listDBs, deleteInstances } from "../index";
+import { TextileClient, getInstancesByQuery, createInstances, deleteInstances } from "../index";
 import { ThreadID, Query } from "@textile/hub";
 
 export default async function instancesHandler(req, res) {
     if (req.method === "GET") {
-        const client = await new TextileClient().init();
-        const query = new Query().orderByIDDesc();
-        const instances = await getInstancesByQuery(client, ThreadID.fromString(req.query.threadID), req.query.collectionName, query)
-        res.status(200).json(instances);
-    } else if (req.method === "POST") {
-        const client = await new TextileClient().init();
-        const json = JSON.parse(req.body);
-        console.log("json", json);
-        const { collectionName, threadID, instance } = json;
-        const newInstance = await createInstances(client, ThreadID.fromString(threadID), collectionName, [instance]);
-        res.status(200).json(newInstance);
-    } else if (req.method === "DELETE") {
-        const client = await new TextileClient().init();
-        const json = JSON.parse(req.body);
-        const { collectionName, threadID, IDs } = json;
         try {
+            const client = await new TextileClient().init();
+            const query = new Query().orderByIDDesc();
+            const instances = await getInstancesByQuery(client, ThreadID.fromString(req.query.threadID), req.query.collectionName, query);
+            res.status(200).json(instances);
+        } catch (e) {
+            console.log(e);
+            res.status(500).json({ "message": `Error fetching instances.` });
+        }
+    } else if (req.method === "POST") {
+        try {
+            const client = await new TextileClient().init();
+            const json = JSON.parse(req.body);
+            const { collectionName, threadID, instance } = json;
+            const newInstance = await createInstances(client, ThreadID.fromString(threadID), collectionName, [instance]);
+            res.status(200).json(newInstance);
+        } catch (e) {
+            console.log(e);
+            res.status(500).json({ "message": `Error creating instance.` });
+        }
+    } else if (req.method === "DELETE") {
+        try {
+            const client = await new TextileClient().init();
+            const json = JSON.parse(req.body);
+            const { collectionName, threadID, IDs } = json;
             await deleteInstances(client, ThreadID.fromString(threadID), collectionName, IDs);
             res.status(200).json({ "message": `Successfully deleted IDs: ${IDs}` });
         } catch (e) {
             console.log(e);
-            res.status(500).json({ "message": `Error deleting IDs: ${IDs}` });
+            res.status(500).json({ "message": `Error deleting instances.` });
         }
+    } else {
+        res.status(500).json({ "message": `Wrong API protocol.` });
     }
 };
