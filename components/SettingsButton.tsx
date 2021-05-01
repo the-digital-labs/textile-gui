cf0e7d7e789b6a46f57f24b2f82802d97323b25f
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-04-30 23:37:14 -0700

    more settings UI layout

--- components/SettingsButton.tsx
+++ components/SettingsButton.tsx
@@ -1,10 +1,19 @@
 import { useContext } from "react";
-import { Modal, Button } from 'antd';
+import { Modal, Button, Form, Input, Switch } from 'antd';
 import { AppContext } from "../store/app";
 import { SettingOutlined } from "@ant-design/icons";
 
 export default function SettingsButton(props) {
     const [appCtxState, appCtxActions] = useContext(AppContext);
+    const [form] = Form.useForm();
+
+    const handleValuesChange = (values) => {
+        console.log(values);
+    };
+
+    const handleSaveSettings = () => {
+
+    };
 
     return (
         <>
@@ -18,10 +27,30 @@ export default function SettingsButton(props) {
                 visible={appCtxState.isSettingsOpen}
                 onOk={() => appCtxActions.setIsSettingsOpen(false)}
                 onCancel={() => appCtxActions.setIsSettingsOpen(false)}
+                footer={[
+                    <Button key="cancel" onClick={() => appCtxActions.setIsSettingsOpen(false)}>
+                        Cancel
+                    </Button>,
+                    <Button key="submit" type="primary" onClick={handleSaveSettings}>
+                        Save
+                    </Button>
+                ]}
             >
-                <p>Hub Key:</p>
-                <p>Hub Secret:</p>
-                <p>Dark Mode:</p>
+                <Form
+                    form={form}
+                    layout="vertical"
+                    onValuesChange={handleValuesChange}
+                >
+                    <Form.Item label="Hub Key" required>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label="Hub Secret" required>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label="UI Theme" required>
+                        <Switch checkedChildren="Light" unCheckedChildren="Dark" defaultChecked />
+                    </Form.Item>
+                </Form>
             </Modal>
         </>
     );
