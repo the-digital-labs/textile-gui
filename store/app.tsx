87527063880135659bf2568e1e4013560c814d7c
ChristopherTrimboli <christopher.trimboli@protonmail.com>
2021-05-01 18:16:34 -0700

    add settings state and actions

--- store/app.tsx
+++ store/app.tsx
@@ -1,12 +1,18 @@
 import { useReducer, createContext } from "react";
 
 const initialState = {
+    hubKey: null,
+    hubSecret: null,
+    isDarkMode: false,
     isTableLoading: false,
     isTreeLoading: false,
     isSettingsOpen: false
 };
 
 const actionTypes = {
+    SET_HUB_KEY: "SET_HUB_KEY",
+    SET_HUB_SECRET: "SET_HUB_SECRET",
+    SET_DARK_MODE: "SET_DARK_MODE",
     SET_TABLE_LOADING: "SET_TABLE_LOADING",
     SET_TREE_LOADING: "SET_TREE_LOADING",
     SET_SETTINGS_OPEN: "SET_SETTINGS_OPEN"
@@ -14,6 +20,15 @@ const actionTypes = {
 
 const reducer = (state, action) => {
     switch (action.type) {
+        case actionTypes.SET_HUB_KEY: {
+            return { ...state, hubKey: action.payload.hubKey };
+        };
+        case actionTypes.SET_HUB_SECRET: {
+            return { ...state, hubSecret: action.payload.hubSecret };
+        };
+        case actionTypes.SET_DARK_MODE: {
+            return { ...state, isDarkMode: action.payload.isDarkMode };
+        };
         case actionTypes.SET_TABLE_LOADING: {
             return { ...state, isTableLoading: action.payload.isTableLoading };
         };
@@ -35,13 +50,31 @@ export function AppProvider({ children }) {
     const [state, dispatch] = useReducer(reducer, initialState);
 
     const actions = {
-        setIsTableLoading: (isTableLoading) => dispatch({
+        setHubKey: (hubKey: string) => dispatch({
+            type: actionTypes.SET_HUB_KEY,
+            payload: {
+                hubKey: hubKey
+            }
+        }),
+        setHubSecret: (hubSecret: string) => dispatch({
+            type: actionTypes.SET_HUB_SECRET,
+            payload: {
+                hubSecret: hubSecret
+            }
+        }),
+        setIsDarkMode: (isDarkMode: boolean) => dispatch({
+            type: actionTypes.SET_DARK_MODE,
+            payload: {
+                isDarkMode: isDarkMode
+            }
+        }),
+        setIsTableLoading: (isTableLoading: boolean) => dispatch({
             type: actionTypes.SET_TABLE_LOADING,
             payload: {
                 isTableLoading: isTableLoading
             }
         }),
-        setIsTreeLoading: (isTreeLoading) => dispatch({
+        setIsTreeLoading: (isTreeLoading: boolean) => dispatch({
             type: actionTypes.SET_TREE_LOADING,
             payload: {
                 isTreeLoading: isTreeLoading
