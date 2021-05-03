c7fb9e3ce3877d2743c10514b0f0b0765e7b6293
ChristopherTrimboli <christopher.trimboli@protonmail.com>
2021-05-02 22:43:20 -0700

    remove darkmode for now (buggy ant forms)

--- components/SettingsForm.tsx
+++ components/SettingsForm.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from "react";
-import { Form, Input, Switch, Button } from 'antd';
+import { Form, Input, Button } from 'antd';
 import { AppContext } from "../store/app";
 
 export default function SettingsForm(props) {
@@ -10,17 +10,14 @@ export default function SettingsForm(props) {
         const values = form.getFieldsValue(true);
         window.localStorage.setItem("HUB_KEY", values.hubKey);
         window.localStorage.setItem("HUB_SECRET", values.hubSecret);
-        window.localStorage.setItem("DARK_MODE", values.darkMode);
         appCtxActions.setHubKey(values.hubKey);
         appCtxActions.setHubSecret(values.hubSecret);
-        appCtxActions.setIsDarkMode(values.darkMode);
     };
 
     useEffect(() => {
         form.setFieldsValue({
             hubKey: appCtxState.hubKey,
             hubSecret: appCtxState.hubSecret,
-            darkMode: appCtxState.darkMode
         });
     }, [appCtxState.isSettingsOpen]);
 
@@ -35,9 +32,6 @@ export default function SettingsForm(props) {
             </Form.Item>
             <Form.Item label="Hub Secret" name="hubSecret" required>
                 <Input />
-            </Form.Item>
-            <Form.Item label="UI Theme" name="darkMode">
-                <Switch checkedChildren="Dark" unCheckedChildren="Light" />
             </Form.Item>
             <Form.Item>
                 <Button type="primary" htmlType="submit">
