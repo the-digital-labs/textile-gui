aba80892f510fefb766a77bf9dfc8c2a610c3f9f
ChristopherTrimboli <christopher.trimboli@protonmail.com>
2021-05-05 01:32:46 -0700

    rewrite top Menu bar without ant, make simpler

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import "../styles/components/TopBar.css";
-import { Menu } from 'antd';
 import { GithubOutlined } from "@ant-design/icons";
 
 export default function TopBar() {
-    return <Menu mode="horizontal" selectable={false}>
-        <Menu.Item key="title">
-            <h2 style={{ marginBottom: 0 }}>textile-gui</h2>
-        </Menu.Item>
-        <Menu.Item key="github" style={{ float: "right" }}>
-            <a href="https://github.com/the-digital-labs/textile-gui" target="_blank">
-                <GithubOutlined style={{ fontSize: 20 }} /> Github
+    return (
+        <div className="topBar">
+            <h2 className="brandName">textile-gui</h2>
+            <a href="https://github.com/the-digital-labs/textile-gui" target="_blank" className="githubIcon">
+                <GithubOutlined style={{ fontSize: 20 }} />
             </a>
-        </Menu.Item>
-    </Menu>
+        </div>
+    );
 };
